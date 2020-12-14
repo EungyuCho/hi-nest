26e2edcc23fb3454dc7f3bdf4e64018ea89e4a16
gyu <cho2304@naver.com>
2020-12-14 18:25:10 +0900

    5. how to unit test in nestjs?

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,27 +1,21 @@
 import {Body, Controller, Delete, Get, Param, Patch, Post, Query} from '@nestjs/common';
-import {MoviesService} from "./movies.service";
-import {Movie} from "./entities/movie.entity";
-import {CreateMovieDto} from "./dto/create-movie.dto";
-import {UpdateMovieDto} from "./dto/update-movie.dto";
+
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
-
-    constructor(private readonly moviesService: MoviesService) { }
+    constructor(private readonly moviesService: MoviesService) {}
 
     @Get()
-    getAll(): Movie[]{
+    getAll(): Movie[] {
         return this.moviesService.getAll();
     }
 
-    @Get("search")
-    search(@Query('year') searchingYear: string) {
-        return `We are search for a movie made after: ${searchingYear}`;
-    }
-
-    @Get(":id")
-    getOne(@Param('id') movieId:number) {
-        console.log(typeof movieId);
+    @Get(':id')
+    getOne(@Param('id') movieId: number): Movie {
         return this.moviesService.getOne(movieId);
     }
 
@@ -30,14 +24,13 @@ export class MoviesController {
         return this.moviesService.create(movieData);
     }
 
-    @Delete(":id")
+    @Delete(':id')
     remove(@Param('id') movieId: number) {
         return this.moviesService.deleteOne(movieId);
     }
 
     @Patch(':id')
-    patch(@Param('id') movieId:number, @Body() updateData: UpdateMovieDto) {
+    patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
         return this.moviesService.update(movieId, updateData);
     }
-
 }
